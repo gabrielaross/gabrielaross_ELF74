bb31cfcaa08bfef8fd9840e7050afba9d16a0d44
gabrielaross <gabrielaross@alunos.utfpr.edu.br>
2021-10-21 09:35:46 -0300

    Correct blinky

--- Lab2/src/blinky.c
+++ Lab2/src/blinky.c
@@ -24,12 +24,17 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
 #include "inc/hw_memmap.h"
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
 
-
+#define SYSTEM_CLOCK_FREQ 10000000
 //*****************************************************************************
 //
 //! \addtogroup example_list
@@ -45,6 +50,7 @@
 // The error routine that is called if the driver library encounters an error.
 //
 //*****************************************************************************
+
 #ifdef DEBUG
 void
 __error__(char *pcFilename, uint32_t ui32Line)
@@ -58,56 +64,63 @@ __error__(char *pcFilename, uint32_t ui32Line)
 // Blink the on-board LED.
 //
 //*****************************************************************************
-int
-main(void)
+
+volatile uint32_t IntCounter = 0;
+
+// Interruption Counter
+void SysTickIntHandler(void)
 {
-    volatile uint32_t ui32Loop;
-
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-
-    //
-    // Check if the peripheral access is enabled.
-    //
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
-    {
-    }
-
-    //
-    // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
-
-    //
-    // Loop forever.
-    //
-    while(1)
-    {
-        //
-        // Turn on the LED.
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-
-        //
-        // Turn off the LED.
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-    }
+    IntCounter++;
 }
+ 
+int
+main(void){
+    uint32_t PinRead;
+    uint32_t SysTickRead;
+    uint32_t ClockPress;   
+	
+    //Peripherals setup = Button
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); //enables a peripheral used for the button
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)){ //determines if a peripheral is ready
+    }
+    
+    //Peripherals setup = LED
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION); //enables a peripheral used for the led
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)){ //determines if a peripheral is ready
+    }
+    
+    //Sets in and out
+    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0); //configures pin for use as GPIO outputs
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1); //configures pin for use as GPIO outputs
+    GPIOPadConfigSet(GPIO_PORTJ_BASE ,GPIO_PIN_0,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU); //get the pad configuration for a pin
+   
+    //Systick setup and interrupt
+    IntMasterEnable(); //enables the processor interrupt
+    SysTickPeriodSet(SYSTEM_CLOCK_FREQ);  //sets the period of the counter
+    SysTickIntRegister(SysTickIntHandler); //registers an interrupt handler for the systick interrupt
+    SysTickEnable(); //enables the counter
+    SysTickIntEnable(); //enables the interrupt
+    
+    IntCounter = 0;
+    
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0x0); //writes a value to the specified pin
+    //(base address, bit-packed representation of the pins, value to write to the pins)
+    while(IntCounter < 12){//1 second to turn the led on
+    }
+    
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1); //writes a value to the specified pin
+    //(base address, bit-packed representation of the pins, value to write to the pins)
+    PinRead = GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_0); //reads the value of the specified pins
+    SysTickRead = SysTickValueGet(); //gets the value of the counter
+    IntCounter = 0;
+    
+    while(PinRead == 1 && IntCounter < 36){ //while not pressed, until 3 seconds
+      if (IntCounter >= 36){
+        printf("There was no interruption under 3 seconds \n");
+      }
+      PinRead = GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_0); //reads the value of the specified pins 
+    }
+    
+    ClockPress = SysTickValueGet() - SysTickRead + (SYSTEM_CLOCK_FREQ * IntCounter); //gets the value of the counter
+    printf("Clocks: %d\n", ClockPress);
+ }
